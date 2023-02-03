d2a5757c615db39a0c0235f3306443a77f0254d0
Mikhail Beliakov <mr.mikhail-beliakov@yandex.ru>
2023-02-03 23:25:20 +0400

    state was added in PizzaBlock

--- src/App.js
+++ src/App.js
@@ -189,16 +189,16 @@ function App() {
 
   return (
     <div className="App">
-      <div class="wrapper">
+      <div className="wrapper">
         <Header />
-        <div class="content">
-          <div class="container">
-            <div class="content__top">
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
               <Categories />
               <Sort />
             </div>
-            <h2 class="content__title">Все пиццы</h2>
-            <div class="content__items">
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
               { pizzaBlocksArr }
             </div>
           </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Categories() {
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
         <li class="active">Все</li>
         <li>Мясные</li>
