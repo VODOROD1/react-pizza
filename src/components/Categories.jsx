dc7ee5eaf9b84b414673dccab8f1a2603c9d9bae
Mikhail Beliakov <mr.mikhail-beliakov@yandex.ru>
2023-02-04 08:46:45 +0400

    different images were added for pizza

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Categories() {
-  return (
-    <div className="categories">
-      <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
-      </ul>
-    </div>
-  );
+    let [activeIndex, setActiveIndex] = useState(0);
+    const categoriesData = [
+        'Все',
+        'Мясные',
+        'Вегетарианская',
+        'Гриль',
+        'Острые',
+        'Закрытые'
+    ]
+
+    function onActive(newActiveIndex) {
+        setActiveIndex(newActiveIndex);
+    }
+
+    return (
+        <div className="categories">
+        <ul>
+            {
+                categoriesData.map((categoryName, index) => {
+                    return (
+                        <li onClick={() => onActive(index)} className={activeIndex == index ? 'active' : ''} key={index}>{categoryName}</li>
+                    )
+                })
+            }
+        </ul>
+        </div>
+    );
 }
 
 export default Categories;
